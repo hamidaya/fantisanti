9b6cff360b4b764464dd3c6e3c53b09e318a0b67
pa38ny <hamid.ayachi@ing.com>
2023-05-23 22:14:57 +0200

    styling button

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,30 +3,31 @@ import { NavLink } from 'react-router-dom';
 import "./Footer.css";
 
 function Footer() {
-
+    const handleNavLinkClick = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
 
     return (
         <div id="footer" className="outer-footer-container">
             <div className="inner-footer-container">
                 <ul>
                     <li>
-                        <NavLink to="/contact" className="default-link" >
+                        <NavLink to="/contact" className="default-link" onClick={handleNavLinkClick}>
                             CONTACT
-
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/about" className="default-link">
+                        <NavLink to="/about" className="default-link" onClick={handleNavLinkClick}>
                             ABOUT
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/termsConditions" className="default-link">
+                        <NavLink to="/termsConditions" className="default-link" onClick={handleNavLinkClick}>
                             TERMS & CONDITIONS
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/privacyCookies" className="default-link">
+                        <NavLink to="/privacyCookies" className="default-link" onClick={handleNavLinkClick}>
                             PRIVACY & COOKIES
                         </NavLink>
                     </li>
